606e90f89b1bb9b794dad544551deeb4dd5ae4fa
vishwabandita <86146138+vishwabandita@users.noreply.github.com>
2025-03-12 14:32:43 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -92,6 +92,10 @@ images_urls = {
                 'image_piperidine': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_piperidine.png'],
                 'image_gamma-picoline': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_gamma-picoline.png'],
                 'image_pyridine': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_pyridine.png'],
+                'image_pyridine-carbon-footprint': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_pyridine-carbon-footprint.png'],
+                'image_3-cyano-pyridine-carbon-footprint': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_3-cyano-pyridine-carbon-footprint.png'],
+                'image_ethical-procedures-chart': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_ethical-procedures-chart.png'],
+                'image_project-management': ['https://storage.googleapis.com/public_thrifty_storage_bucket/jubilant/image_project-management.png'],
 
                 #Ather Images
                 'image_ather_rizta-s': ['https://storage.googleapis.com/public_thrifty_storage_bucket/ather/image_ather_rizta-s.png'],
